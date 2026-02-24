ff4b08deb59330e6c39a5ddf0722cc6a48fe7edb
syed02-devops <syediliyas021998@gmail.com>
2026-02-24 16:02:38 +0530

    Initial commit - Dockerized Node app

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require("http");
+
+http.createServer((req, res) => {
+  res.write("DevOps CI/CD is Working 🚀");
+  res.end();
+}).listen(3000);
+
+console.log("Server running on port 3000");
